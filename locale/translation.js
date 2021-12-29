75708aa1834ddf5c5b92db2ad6b2b4c817690e83
obijuan <obijuan.cube@gmail.com>
2021-12-29 17:18:40 +0100

    Initial commit. Example 01 for Alhambra-II board

---
+++ locale/translation.js
@@ -0,0 +1,31 @@
+// Translation document for the collection
+// =======================================
+// This file contains the texts
+// annotated for translation
+//
+// Instructions:
+// 1. Open the PO file with Poedit
+// 2. Press "Update" to update from sources
+
+gettext('Alhambra-II');
+gettext('01-LEDs-buttons');
+gettext('Constant bit 1');
+gettext('NOT gate (Verilog implementation)');
+gettext('Constant bit 0');
+gettext('Generic: 4-bits generic constant (0-15)');
+gettext('2-to-1 Multplexer (4-bit channels)');
+gettext('2-to-1 Multplexer (1-bit channels)');
+gettext('OR2: Two bits input OR gate');
+gettext('Two bits input And gate');
+gettext('Bus4-Join-all: Join all the wires into a 4-bits Bus');
+gettext('Bus4-Split-all: Split the 4-bits bus into its wires');
+gettext('### Turn on LED7');
+gettext('### Change LEDs 5 and 6 when pressing sw1 button');
+gettext('### Turn off LED4');
+gettext('### Display two constants on 4 LEDs');
+gettext('Press for displaying the  \nother constant on the LEDs');
+gettext('Input');
+gettext('Output');
+gettext('Channel A');
+gettext('Channel B');
+gettext('01-LEDs-buttons');
